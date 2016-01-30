5c98de420cd0f6942d1fc356936353370ee12834
darudeSlurpstorm <davis.jack@husky.neu.edu>
2016-01-30 13:54:46 -0500

    Flying text still not working :c

---
+++ Assets/Scripts/FlyingText.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingText : MonoBehaviour {
+
+	public string myString;
+
+	public float FADE_SPEED = 0.1f;
+	public float FLY_SPEED = 0.001f;
+
+	private Rigidbody2D myRigidbody;
+	private TextMesh myText;
+
+	// Use this for initialization
+	void Start () {
+
+		myRigidbody = this.GetComponent<Rigidbody2D>();
+		myText = this.GetComponent<TextMesh>();
+
+		myText.text = myString;
+
+		Vector2 myVel = myRigidbody.velocity;
+		myVel.y = FLY_SPEED;
+		myRigidbody.velocity = myVel;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Color temp = myText.color;
+		temp.a -= 0.01f;
+		myText.color = temp;
+	
+	}
+
+	public void setText(string toSet, Color newColor) {
+
+		myString = toSet;
+
+	}
+}
