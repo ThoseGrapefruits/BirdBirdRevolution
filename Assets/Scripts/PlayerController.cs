fd8ef2a5e675019425c069d20936145ca403a89a
Lucas Pickering <pickering.l@husky.neu.edu>
2016-01-29 23:30:59 -0500

    Fixed Jack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-	// Store an instance of a beat object
-	[SerializeField] private GameObject theBeat;
+    // Store an instance of a beat object
+    [SerializeField]
+    private GameObject theBeat;
 
-	// Constants
-	private int OFF_BEAT_COMBO;
-	private int ON_BEAT_COMBO;
-	
-	// Glob variables
-	public int score;
+    // Constants
+    private int OFF_BEAT_COMBO;
+    private int ON_BEAT_COMBO;
+
+    // Glob variables
+    public int score;
 
     private enum Move
     {
@@ -19,7 +20,7 @@ public class PlayerController : MonoBehaviour
     };
 
     private Dictionary<string, string> combos = new Dictionary<string, string>();
-    private LinkedList<Move> activeCombo = new LinkedList<Move>();
+    private string activeCombo = "";
 
     void Start()
     {
@@ -28,34 +29,34 @@ public class PlayerController : MonoBehaviour
         combos.Add("Wavedash Left", "LLD");
         combos.Add("Split", "DD");
 
-		// You get 5 points for landing a combo off-beat (subject to changes)
-		// You get 10 points for landing a combo on-beat (subject to changes)
-		OFF_BEAT_COMBO = 5;
-		ON_BEAT_COMBO = 10;
+        // You get 5 points for landing a combo off-beat (subject to changes)
+        // You get 10 points for landing a combo on-beat (subject to changes)
+        OFF_BEAT_COMBO = 5;
+        ON_BEAT_COMBO = 10;
 
-		// Duh, count your current score
-		score = 0;
+        // Duh, count your current score
+        score = 0;
     }
 
     void Update()
     {
         if (Input.GetButtonDown("Up"))
         {
-            activeCombo.AddLast(Move.U);
+            activeCombo += Move.U;
         }
         if (Input.GetButtonDown("Down"))
         {
-            activeCombo.AddLast(Move.D);
+            activeCombo += Move.D;
         }
         if (Input.GetButtonDown("Left"))
         {
-            activeCombo.AddLast(Move.L);
+            activeCombo += Move.L;
         }
         if (Input.GetButtonDown("Right"))
         {
-            activeCombo.AddLast(Move.R);
+            activeCombo += Move.R;
         }
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             ClearCombo();
         }
@@ -63,32 +64,12 @@ public class PlayerController : MonoBehaviour
         CheckForCombo();
     }
 
-    private LinkedList<Move> ComboStringToList(string combo)
-    {
-        LinkedList<Move> toReturn = new LinkedList<Move>();
-        foreach (char c in combo.ToCharArray())
-        {
-            toReturn.AddLast((Move)c); // Get move corresponding to the character c
-        }
-        return toReturn;
-    }
-
-    private string ComboListToString(LinkedList<Move> combo)
-    {
-        string s = "";
-        foreach (Move move in combo)
-        {
-            s += (char)move;
-        }
-        return s;
-    }
-
     private void CheckForCombo()
     {
         foreach (string combo in combos.Values)
         {
             // If this combo matches the active combo
-            if (combo.Length == activeCombo.Count && combo.Equals(ComboListToString(activeCombo)))
+            if (combo.Equals(activeCombo))
             {
                 CompleteCombo(combo); // We made it boys
             }
@@ -97,21 +78,23 @@ public class PlayerController : MonoBehaviour
 
     private void CompleteCombo(string combo)
     {
-        //Debug.Log(combo);
+        Debug.Log(combo);
 
-		if (theBeat.GetComponent<Beat>().comboTime) {
-			score += ON_BEAT_COMBO;
-		} else {
-			score += OFF_BEAT_COMBO;
-		}
+        if (theBeat.GetComponent<Beat>().comboTime)
+        {
+            score += ON_BEAT_COMBO;
+        }
+        else {
+            score += OFF_BEAT_COMBO;
+        }
 
-		Debug.Log (score);
+        Debug.Log(score);
 
         ClearCombo();
     }
 
     private void ClearCombo()
     {
-        activeCombo.Clear();
+        activeCombo = "";
     }
 }
