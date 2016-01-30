64d3eb5d4b0172e737ac45c173fdf37dffb080e6
darudeSlurpstorm <davis.jack@husky.neu.edu>
2016-01-29 23:45:47 -0500

    Start text script

---
+++ Assets/Scripts/ComboDisplay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ComboDisplay : MonoBehaviour {
+
+	// Keep a reference to the burd
+	[SerializeField] private GameObject theBird;
+
+	private Text textComp = this.GetComponent<Text>();
+	private PlayerController playerComp = theBird.GetComponent<PlayerController>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		textComp.text = playerComp.GetActiveCombo();
+	
+	}
+}
