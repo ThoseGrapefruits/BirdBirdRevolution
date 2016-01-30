bfc83e3308c47800a79460fdf0c841a7e6c1b2ff
Lucas Pickering <pickering.l@husky.neu.edu>
2016-01-29 21:14:44 -0500

    Started PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private enum Move
+    {
+        U, D, L, R
+    };
+
+    private Dictionary<string, string> combos = new Dictionary<string, string>();
+    private LinkedList<Move> activeCombo = new LinkedList<Move>();
+
+    void Start()
+    {
+        combos.Add("Twirl", "LRLR");
+        combos.Add("Wavedash Right", "RRD");
+        combos.Add("Wavedash Left", "LLD");
+        combos.Add("Split", "DD");
+    }
+
+    void Update()
+    {
+        Debug.Log(Input.GetKey("Up"));
+    }
+}
