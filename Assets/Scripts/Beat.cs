b36ea75be68a0a7d9132e293f635e75fb921c8ea
darudeSlurpstorm <davis.jack@husky.neu.edu>
2016-01-29 21:24:25 -0500

    teamwork programming bro

---
+++ Assets/Scripts/Beat.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beat : MonoBehaviour {
+
+	private float TIME;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+
+		TIME = 3;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		StartCoroutine (Countdown());
+	
+	}
+
+	IEnumerator Countdown() {
+
+		for (timer = TIME; timer >= 0; timer -= Time.deltaTime) {
+			yield return 0;
+		}
+
+		Debug.Log ("derpderpderp");
+
+	}
+}
