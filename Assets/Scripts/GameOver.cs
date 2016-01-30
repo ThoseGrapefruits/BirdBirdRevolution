e4138901b5875474286cda21432c4c70b0a728b1
Lucas Pickering <pickering.l@husky.neu.edu>
2016-01-30 13:57:09 -0500

    Added game over stuff

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene("Scenes/real_scene_pls_notice");
+        }
+    }
+}
